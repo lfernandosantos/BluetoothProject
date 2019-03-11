5c3b4b295ad198aaec27ffc5e36c0443b9346323
Fernando Santos <lsantos@muxi.com.br>
2019-03-11 20:17:44 -0300

    Create Pinpad struct.

--- BluetoothPW/model/Pinpad.swift
+++ BluetoothPW/model/Pinpad.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+import CoreBluetooth
+
+enum PinpadCommands: String {
+    case open           = "OPN"
+    case getInfo        = "GIN"
+    case display        = "DSP"
+    case close          = "CLO"
+    case getCard        = "GCR"
+    case goOnChip       = "GOC"
+    case getTimeStamp   = "GTS"
+    case tableLoadInit  = "TLI"
+    case tableLoadRec   = "TLR"
+    case tableLoadEnd   = "TLE"
+    case finishChip     = "FNC"
+    case nak            = "NAK"
+    case error
+    
+    static func getFromString(function: String?) -> PinpadCommands {
+        if let function = function, let command = PinpadCommands.init(rawValue: function) {
+            return command
+        } else {
+            return self.error
+        }
+    }
+}
+
+
+struct PinPad {
+    
+    var device: CBPeripheral
+    
+    func readData(data: Data) {
+        
+    }
+    
+    
+    func loadTable(){
+        
+    }
+    
+    func finishLoadTable(){
+        
+    }
+}
